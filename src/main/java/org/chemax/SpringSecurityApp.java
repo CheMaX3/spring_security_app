4028056fedd7dc44ecfd5630a845c1ed4ca741ab
CheMaX <chemax05@mail.ru>
2023-07-30 14:37:51 +0300

    Start spring security project

---
+++ src/main/java/org/chemax/SpringSecurityApp.java
@@ -0,0 +1,11 @@
+package org.chemax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityApp {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityApp.class, args);
+    }
+}
---
+++ src/main/java/org/chemax/services/PersonDetailsService.java
@@ -0,0 +1,33 @@
+package org.chemax.services;
+
+import org.chemax.models.Person;
+import org.chemax.repositories.PeopleRepository;
+import org.chemax.security.PersonDetails;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PersonDetailsService implements UserDetailsService {
+
+    private final PeopleRepository peopleRepository;
+
+    @Autowired
+    public PersonDetailsService(PeopleRepository peopleRepository) {
+        this.peopleRepository = peopleRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        Optional<Person> person = peopleRepository.findByUsername(s);
+
+        if (person.isEmpty())
+            throw new UsernameNotFoundException("User not found");
+
+        return new PersonDetails(person.get());
+    }
+}
